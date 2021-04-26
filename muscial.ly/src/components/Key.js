330c2e0dcc8dacb1861355fa1d859fefb0962b74
Seth Hogue <shogue21@basecampcodingacademy.org>
2021-04-26 10:23:46 -0500

    Definitely a piano

---
+++ muscial.ly/src/components/Key.js
@@ -0,0 +1,9 @@
+const Key = ({ note }) => {
+  return (
+    <div>
+      <button className="note">{note}</button>
+    </div>
+  );
+};
+
+export default Key;
---
+++ muscial.ly/src/components/Piano.js
@@ -0,0 +1,49 @@
+import Key from "./Key";
+
+const Piano = () => {
+  return (
+    <div>
+      <h3>This is definitely a piano.</h3>
+      <div className="notes">
+        <Key note="c2" />
+        <Key note="d2" />
+        <Key note="e2" />
+        <Key note="f2" />
+        <Key note="g2" />
+        <Key note="a2" />
+        <Key note="b2" />
+        <Key note="c3" />
+        <Key note="d3" />
+        <Key note="e3" />
+        <Key note="f3" />
+        <Key note="g3" />
+        <Key note="a3" />
+        <Key note="b3" />
+        <Key note="c4" />
+        <Key note="d4" />
+        <Key note="e4" />
+        <Key note="f4" />
+        <Key note="g4" />
+        <Key note="a4" />
+        <Key note="b4" />
+        <Key note="c5" />
+        <Key note="d5" />
+        <Key note="e5" />
+        <Key note="f5" />
+        <Key note="g5" />
+        <Key note="a5" />
+        <Key note="b5" />
+        <Key note="c6" />
+        <Key note="d6" />
+        <Key note="e6" />
+        <Key note="f6" />
+        <Key note="g6" />
+        <Key note="a6" />
+        <Key note="b6" />
+        <Key note="c7" />
+      </div>
+    </div>
+  );
+};
+
+export default Piano;
