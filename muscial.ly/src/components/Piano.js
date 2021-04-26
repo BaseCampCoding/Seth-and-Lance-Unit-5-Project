0c20d5ebf85d3aaff951c4b691d961bc91271516
Lance Easley <leasley21@basecampcodingacademy.org>
2021-04-26 14:19:53 -0500

    Renamed note parameters

--- muscial.ly/src/components/Piano.js
+++ muscial.ly/src/components/Piano.js
@@ -6,64 +6,64 @@ const Piano = () => {
       <h3>This is definitely a piano.</h3>
       <div className="notes">
         <Key type="note-white" note="c2" />
-        <Key type="note-black" note="c#2" />
+        <Key type="note-black" note="c2#" />
         <Key type="note-white" note="d2" />
-        <Key type="note-black" note="d#2" />
+        <Key type="note-black" note="d2#" />
         <Key type="note-white" note="e2" />
         <Key type="note-white" note="f2" />
-        <Key type="note-black" note="f#2" />
+        <Key type="note-black" note="f2#" />
         <Key type="note-white" note="g2" />
-        <Key type="note-black" note="g#2" />
+        <Key type="note-black" note="g2#" />
         <Key type="note-white" note="a2" />
-        <Key type="note-black" note="a#2" />
+        <Key type="note-black" note="a2#" />
         <Key type="note-white" note="b2" />
         <Key type="note-white" note="c3" />
-        <Key type="note-black" note="c#3" />
+        <Key type="note-black" note="c3#" />
         <Key type="note-white" note="d3" />
-        <Key type="note-black" note="d#3" />
+        <Key type="note-black" note="d3#" />
         <Key type="note-white" note="e3" />
         <Key type="note-white" note="f3" />
-        <Key type="note-black" note="f#3" />
+        <Key type="note-black" note="f3#" />
         <Key type="note-white" note="g3" />
-        <Key type="note-black" note="g#3" />
+        <Key type="note-black" note="g3#" />
         <Key type="note-white" note="a3" />
-        <Key type="note-black" note="a#3" />
+        <Key type="note-black" note="a3#" />
         <Key type="note-white" note="b3" />
         <Key type="note-white" note="c4" />
-        <Key type="note-black" note="c#4" />
+        <Key type="note-black" note="c4#" />
         <Key type="note-white" note="d4" />
-        <Key type="note-black" note="d#4" />
+        <Key type="note-black" note="d4#" />
         <Key type="note-white" note="e4" />
         <Key type="note-white" note="f4" />
-        <Key type="note-black" note="f#4" />
+        <Key type="note-black" note="f4#" />
         <Key type="note-white" note="g4" />
-        <Key type="note-black" note="g#4" />
+        <Key type="note-black" note="g4#" />
         <Key type="note-white" note="a4" />
-        <Key type="note-black" note="a#4" />
+        <Key type="note-black" note="a4#" />
         <Key type="note-white" note="b4" />
         <Key type="note-white" note="c5" />
-        <Key type="note-black" note="c#5" />
+        <Key type="note-black" note="c5#" />
         <Key type="note-white" note="d5" />
-        <Key type="note-black" note="d#5" />
+        <Key type="note-black" note="d5#" />
         <Key type="note-white" note="e5" />
         <Key type="note-white" note="f5" />
-        <Key type="note-black" note="f#5" />
+        <Key type="note-black" note="f5#" />
         <Key type="note-white" note="g5" />
-        <Key type="note-black" note="g#5" />
+        <Key type="note-black" note="g5#" />
         <Key type="note-white" note="a5" />
-        <Key type="note-black" note="a#5" />
+        <Key type="note-black" note="a5#" />
         <Key type="note-white" note="b5" />
         <Key type="note-white" note="c6" />
-        <Key type="note-black" note="c#6" />
+        <Key type="note-black" note="c6#" />
         <Key type="note-white" note="d6" />
-        <Key type="note-black" note="d#6" />
+        <Key type="note-black" note="d6#" />
         <Key type="note-white" note="e6" />
         <Key type="note-white" note="f6" />
-        <Key type="note-black" note="f#6" />
+        <Key type="note-black" note="f6#" />
         <Key type="note-white" note="g6" />
-        <Key type="note-black" note="g#6" />
+        <Key type="note-black" note="g6#" />
         <Key type="note-white" note="a6" />
-        <Key type="note-black" note="a#6" />
+        <Key type="note-black" note="a6#" />
         <Key type="note-white" note="b6" />
         <Key type="note-white" note="c7" />
       </div>
