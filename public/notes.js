367c49715361c827c4167978779a2c51877e7061
Seth Hogue <shogue21@basecampcodingacademy.org>
2021-04-27 15:12:45 -0500

    no repeat

--- public/notes.js
+++ public/notes.js
@@ -10,31 +10,39 @@ for (const note of document.querySelectorAll(".note")) {
 }
 
 window.addEventListener("keydown", (Event) => {
-  if (Event.code && Event.shiftKey) {
-    const sound = new Audio(
-      `/instruments/piano/${pianoKeysTranslator[Event.code]}s.ogg`
-    );
-    sound.volume = 0.1;
-    const key = document.getElementById(`${pianoKeysTranslator[Event.code]}s`);
-    key.classList.add("note-black-pressed");
-    return sound.play();
-  }
+  if (!Event.repeat) {
+    if (Event.code && Event.shiftKey) {
+      const sound = new Audio(
+        `/instruments/piano/${pianoKeysTranslator[Event.code]}s.ogg`
+      );
+      sound.volume = 0.1;
+      const key = document.getElementById(
+        `${pianoKeysTranslator[Event.code]}s`
+      );
+      key.classList.add("note-black-pressed");
+      return sound.play();
+    }
 
-  const sound = new Audio(
-    `/instruments/piano/${pianoKeysTranslator[Event.code]}.ogg`
-  );
-  const key = document.getElementById(`${pianoKeysTranslator[Event.code]}`);
-  key.classList.add("note-white-pressed");
-  sound.volume = 0.1;
-  sound.play();
-  setTimeout(() => {
-    sound.pause();
-  }, 2000);
+    const sound = new Audio(
+      `/instruments/piano/${pianoKeysTranslator[Event.code]}.ogg`
+    );
+    const key = document.getElementById(`${pianoKeysTranslator[Event.code]}`);
+    key.classList.add("note-white-pressed");
+    sound.volume = 0.1;
+    sound.play();
+    setTimeout(() => {
+      sound.pause();
+    }, 2000);
+  }
 });
 
 window.addEventListener("keyup", (Event) => {
-  const naturalKey = document.getElementById(`${pianoKeysTranslator[Event.code]}`);
+  const naturalKey = document.getElementById(
+    `${pianoKeysTranslator[Event.code]}`
+  );
   naturalKey.classList.remove("note-white-pressed");
-  const sharpKey = document.getElementById(`${pianoKeysTranslator[Event.code]}s`);
+  const sharpKey = document.getElementById(
+    `${pianoKeysTranslator[Event.code]}s`
+  );
   sharpKey ? sharpKey.classList.remove("note-black-pressed") : "";
 });
