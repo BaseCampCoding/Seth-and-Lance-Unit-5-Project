541ab1fb4c23684ae381d786ba0e2850ed6e9279
Lance Easley <leasley21@basecampcodingacademy.org>
2021-04-27 11:29:14 -0500

    Added piano key button mapping

--- muscial.ly/src/components/Piano.js
+++ muscial.ly/src/components/Piano.js
@@ -5,67 +5,67 @@ const Piano = () => {
     <>
       <h3>This is definitely a piano.</h3>
       <div className="notes">
-        <Key type="note-white" note="c2" />
-        <Key type="note-black" note="c2s" />
-        <Key type="note-white" note="d2" />
-        <Key type="note-black" note="d2s" />
-        <Key type="note-white" note="e2" />
-        <Key type="note-white" note="f2" />
-        <Key type="note-black" note="f2s" />
-        <Key type="note-white" note="g2" />
-        <Key type="note-black" note="g2s" />
-        <Key type="note-white" note="a2" />
-        <Key type="note-black" note="a2s" />
-        <Key type="note-white" note="b2" />
-        <Key type="note-white" note="c3" />
-        <Key type="note-black" note="c3s" />
-        <Key type="note-white" note="d3" />
-        <Key type="note-black" note="d3s" />
-        <Key type="note-white" note="e3" />
-        <Key type="note-white" note="f3" />
-        <Key type="note-black" note="f3s" />
-        <Key type="note-white" note="g3" />
-        <Key type="note-black" note="g3s" />
-        <Key type="note-white" note="a3" />
-        <Key type="note-black" note="a3s" />
-        <Key type="note-white" note="b3" />
-        <Key type="note-white" note="c4" />
-        <Key type="note-black" note="c4s" />
-        <Key type="note-white" note="d4" />
-        <Key type="note-black" note="d4s" />
-        <Key type="note-white" note="e4" />
-        <Key type="note-white" note="f4" />
-        <Key type="note-black" note="f4s" />
-        <Key type="note-white" note="g4" />
-        <Key type="note-black" note="g4s" />
-        <Key type="note-white" note="a4" />
-        <Key type="note-black" note="a4s" />
-        <Key type="note-white" note="b4" />
-        <Key type="note-white" note="c5" />
-        <Key type="note-black" note="c5s" />
-        <Key type="note-white" note="d5" />
-        <Key type="note-black" note="d5s" />
-        <Key type="note-white" note="e5" />
-        <Key type="note-white" note="f5" />
-        <Key type="note-black" note="f5s" />
-        <Key type="note-white" note="g5" />
-        <Key type="note-black" note="g5s" />
-        <Key type="note-white" note="a5" />
-        <Key type="note-black" note="a5s" />
-        <Key type="note-white" note="b5" />
-        <Key type="note-white" note="c6" />
-        <Key type="note-black" note="c6s" />
-        <Key type="note-white" note="d6" />
-        <Key type="note-black" note="d6s" />
-        <Key type="note-white" note="e6" />
-        <Key type="note-white" note="f6" />
-        <Key type="note-black" note="f6s" />
-        <Key type="note-white" note="g6" />
-        <Key type="note-black" note="g6s" />
-        <Key type="note-white" note="a6" />
-        <Key type="note-black" note="a6s" />
-        <Key type="note-white" note="b6" />
-        <Key type="note-white" note="c7" />
+        <Key type="note-white" keyChar="1" note="c2" />
+        <Key type="note-black" keyChar="!" note="c2s" />
+        <Key type="note-white" keyChar="2" note="d2" />
+        <Key type="note-black" keyChar="@" note="d2s" />
+        <Key type="note-white" keyChar="3" note="e2" />
+        <Key type="note-white" keyChar="4" note="f2" />
+        <Key type="note-black" keyChar="$" note="f2s" />
+        <Key type="note-white" keyChar="5" note="g2" />
+        <Key type="note-black" keyChar="%" note="g2s" />
+        <Key type="note-white" keyChar="6" note="a2" />
+        <Key type="note-black" keyChar="^" note="a2s" />
+        <Key type="note-white" keyChar="7" note="b2" />
+        <Key type="note-white" keyChar="8" note="c3" />
+        <Key type="note-black" keyChar="*" note="c3s" />
+        <Key type="note-white" keyChar="9" note="d3" />
+        <Key type="note-black" keyChar="(" note="d3s" />
+        <Key type="note-white" keyChar="0" note="e3" />
+        <Key type="note-white" keyChar="q" note="f3" />
+        <Key type="note-black" keyChar="Q" note="f3s" />
+        <Key type="note-white" keyChar="w" note="g3" />
+        <Key type="note-black" keyChar="W" note="g3s" />
+        <Key type="note-white" keyChar="e" note="a3" />
+        <Key type="note-black" keyChar="E" note="a3s" />
+        <Key type="note-white" keyChar="r" note="b3" />
+        <Key type="note-white" keyChar="t" note="c4" />
+        <Key type="note-black" keyChar="T" note="c4s" />
+        <Key type="note-white" keyChar="y" note="d4" />
+        <Key type="note-black" keyChar="Y" note="d4s" />
+        <Key type="note-white" keyChar="u" note="e4" />
+        <Key type="note-white" keyChar="i" note="f4" />
+        <Key type="note-black" keyChar="I" note="f4s" />
+        <Key type="note-white" keyChar="o" note="g4" />
+        <Key type="note-black" keyChar="O" note="g4s" />
+        <Key type="note-white" keyChar="p" note="a4" />
+        <Key type="note-black" keyChar="P" note="a4s" />
+        <Key type="note-white" keyChar="a" note="b4" />
+        <Key type="note-white" keyChar="s" note="c5" />
+        <Key type="note-black" keyChar="S" note="c5s" />
+        <Key type="note-white" keyChar="d" note="d5" />
+        <Key type="note-black" keyChar="D" note="d5s" />
+        <Key type="note-white" keyChar="f" note="e5" />
+        <Key type="note-white" keyChar="g" note="f5" />
+        <Key type="note-black" keyChar="G" note="f5s" />
+        <Key type="note-white" keyChar="h" note="g5" />
+        <Key type="note-black" keyChar="H" note="g5s" />
+        <Key type="note-white" keyChar="j" note="a5" />
+        <Key type="note-black" keyChar="J" note="a5s" />
+        <Key type="note-white" keyChar="k" note="b5" />
+        <Key type="note-white" keyChar="l" note="c6" />
+        <Key type="note-black" keyChar="L" note="c6s" />
+        <Key type="note-white" keyChar="z" note="d6" />
+        <Key type="note-black" keyChar="Z" note="d6s" />
+        <Key type="note-white" keyChar="x" note="e6" />
+        <Key type="note-white" keyChar="c" note="f6" />
+        <Key type="note-black" keyChar="C" note="f6s" />
+        <Key type="note-white" keyChar="v" note="g6" />
+        <Key type="note-black" keyChar="V" note="g6s" />
+        <Key type="note-white" keyChar="b" note="a6" />
+        <Key type="note-black" keyChar="B" note="a6s" />
+        <Key type="note-white" keyChar="n" note="b6" />
+        <Key type="note-white" keyChar="m" note="c7" />
       </div>
     </>
   );
