20ebc0893b62aa38f4fc14d072e361898f6c3b8f
Seth Hogue <shogue21@basecampcodingacademy.org>
2021-04-28 11:50:32 -0500

    Instructions for Ghost Play

--- src/components/Piano.js
+++ src/components/Piano.js
@@ -76,20 +76,27 @@ const Piano = () => {
         <Key beginner={beginnerMode} type="note-white" keyChar="m" note="c7" />
       </div>
       <h3>Ghost Play</h3>
-      <textarea id="ghost-play-input" rows="16" cols="72">
-        [6et]| 7| [8yo]| 0| [9r]| 8| [I5y]||
-        [6etu]| 7| [8yoa]| 0| [I9ry]| 8| [I5yp]||
-        [6etu]| 7| [8yoa]| 0| [I9ry]| 8| [I5yp]||
-        [6etu]| [7d]/ f/ [8yoa]| 0/ o. p. [I9ry]| 8/ a. d. [I5yp]||
-        [6etu]/ h/ [7f]/ d/ [8yoa]| 0/ o. p. [I9ry]| 8/ d. a. [I5yp]||
-        [6etu]| [7d]/ f/ [8yoah]| 0/ [ok]. [pj]. [I9ry]| 8/ [ah]. G. [I5ypd]||
-        [6etu]| [7d]/ f/ [8yoa]| 0/ o. p. [I9ry]| 8/ a. d. [I5yp]||
-        [6etu]| [7d]/ f/ [8yoa]| 0/ o. p. [I9ry]| 8/ a. d. [I5yp]||
-        [6etu]| [7d]/ f/ [8yoa]| 0/ o. p. [I9ry]| [8k]/ a. d. [I5yp]||
-        [0uoa]||f/ d/ [T6eup]| p/ o/ [I9ey]||o. p. [8wtu]||
-        [0uoa]||f/ d/ [T6eup]| p/ [oj]/ [I9ey]| h/ [ok]. p. [wtu]
-      </textarea>
-      <br/>
+      <div className="ghostplay">
+        <textarea id="ghost-play-input" rows="16" cols="72">
+          [6et]| 7| [8yo]| 0| [9r]| 8| [I5y]|| [6etu]| 7| [8yoa]| 0| [I9ry]| 8|
+          [I5yp]|| [6etu]| 7| [8yoa]| 0| [I9ry]| 8| [I5yp]|| [6etu]| [7d]/ f/
+          [8yoa]| 0/ o. p. [I9ry]| 8/ a. d. [I5yp]|| [6etu]/ h/ [7f]/ d/ [8yoa]|
+          0/ o. p. [I9ry]| 8/ d. a. [I5yp]|| [6etu]| [7d]/ f/ [8yoah]| 0/ [ok].
+          [pj]. [I9ry]| 8/ [ah]. G. [I5ypd]|| [6etu]| [7d]/ f/ [8yoa]| 0/ o. p.
+          [I9ry]| 8/ a. d. [I5yp]|| [6etu]| [7d]/ f/ [8yoa]| 0/ o. p. [I9ry]| 8/
+          a. d. [I5yp]|| [6etu]| [7d]/ f/ [8yoa]| 0/ o. p. [I9ry]| [8k]/ a. d.
+          [I5yp]|| [0uoa]||f/ d/ [T6eup]| p/ o/ [I9ey]||o. p. [8wtu]||
+          [0uoa]||f/ d/ [T6eup]| p/ [oj]/ [I9ey]| h/ [ok]. p. [wtu]
+        </textarea>
+        <ul>
+          <li>Group Notes with [ ]</li>
+          <li>"," = 100ms rest</li>
+          <li>"." = 250ms rest</li>
+          <li>"/" = 500 ms rest</li>
+          <li>"|" = 1000 ms rest</li>
+        </ul>
+      </div>
+      <br />
       <button id="ghost-play-submit">Play</button>
     </>
   );
