c25c25095b6b29586d100066f065fd2ed21a1742
Seth Hogue <shogue21@basecampcodingacademy.org>
2021-04-28 15:55:56 -0500

    removed console log and comments

--- public/metronome.js
+++ public/metronome.js
@@ -15,29 +15,21 @@ metronomePWR.addEventListener("change", () => {
       wentThrough = true;
 
       var request = new XMLHttpRequest();
-      //open the request
       request.open("GET", url, true);
-      //webaudio paramaters
       request.responseType = "arraybuffer";
-      //Once the request has completed... do this
       request.onload = function () {
         context.decodeAudioData(request.response, function (response) {
-          /* --- play the sound AFTER the buffer loaded --- */
-          //set the buffer to the response we just received.
           if (source.buffer === null) {
             source.buffer = response;
           }
-          //start(0) should play asap.
           source.start();
           source.loop = true;
         });
       };
-      //Now that the request has been defined, actually make the request. (send it)
       request.send();
     }
   }
   if (!metronomePWR.checked) {
-    console.log("help");
     source.disconnect(context.destination);
   }
 });
