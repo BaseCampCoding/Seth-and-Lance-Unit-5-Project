da4cbbbf79ce5f9726d6b046992d20edea6bc518
Seth Hogue <shogue21@basecampcodingacademy.org>
2021-04-30 09:10:00 -0500

    Fixed Damned spacing

--- public/constants.js
+++ public/constants.js
@@ -374,25 +374,25 @@ x.,c.,b.,c.,
 [qet]|/
 [qetu]|/
 [8qeti]|/`,
-  Damned: `[k30]/ f. a. k. f. a. k. f. k. f. a. k. f. a. k. f.
-[l81]/ f. s. l. f. s. l. f. z. f. s. l. f. s. l. f.
-[k30]/ f. a. k. f. a. k. f. k. f. a. k. f. a. k. f.
-[l81]/ f. s. l. f. s. l. f. z. f. s. l. f. s. l. f.
-[xk]/ f. a. k. f. a. k. f. [nk]/ f. a. k. f. a. k. f.
-[ml]/ f. s. l. f. s. l. f. z. f. s. l. f. s. l. f.
-[xk]/ f. a. k. f. a. k. f. [nk]/ f. a. k. f. a. k. f.
-[lb]/ f. s. l. f. s. l. f. z. f. s. l. f. s. l. f.
-[xk]/ f. a. k. f. a. k. f. [nk]/ f. a. k. f. a. k. f.
-[ml]/ f. s. l. f. s. l. f. z. f. s. l. f. s. l. f.
-[xk]/ f. a. k. f. a. k. f. [nk]/ f. a. k. f. a. k. f.
-[lb]/ f. s. l. f. s. l. f. z. f. s. l. f. s. l.
-h,,j,,J,, h. d. J. h. d. J. h. J. h. d. J. h. d. J. h. k. h. f. k. h. f. k. h.
-[xk]/ h. f. [zk]/ h. f. [nk]/ h.
-[JB]/ h. d. J. h. d. J. h. J. h. d. J. h. d. J. h. k. h. f. k. h. f. k. h.
-[xk]/ h. f. [zk]/ h. f. k. h. J. h. d. J. h. d. J. h.
-J. G. d. J. G. d. J. G. J. G. S. J. G. S. J. G. d.
-J. G. d. J. G. S. L. J. G. z. J. G. L. J. G. z. J.
-G. L. J. G. L. J. G. L. J. G. L. J. G. L. J. G. L.`,
+  Damned: `[k30]/f.a.k.f.a.k.f.k.f.a.k.f.a.k.f.
+[l81]/f.s.l.f.s.l.f.z.f.s.l.f.s.l.f.
+[k30]/f.a.k.f.a.k.f.k.f.a.k.f.a.k.f.
+[l81]/f.s.l.f.s.l.f.z.f.s.l.f.s.l.f.
+[xk]/f.a.k.f.a.k.f.[nk]/f.a.k.f.a.k.f.
+[ml]/f.s.l.f.s.l.f.z.f.s.l.f.s.l.f.
+[xk]/f.a.k.f.a.k.f.[nk]/f.a.k.f.a.k.f.
+[lb]/f.s.l.f.s.l.f.z.f.s.l.f.s.l.f.
+[xk]/f.a.k.f.a.k.f.[nk]/f.a.k.f.a.k.f.
+[ml]/f.s.l.f.s.l.f.z.f.s.l.f.s.l.f.
+[xk]/f.a.k.f.a.k.f.[nk]/f.a.k.f.a.k.f.
+[lb]/f.s.l.f.s.l.f.z.f.s.l.f.s.l.
+h,,j,,J,,h.d.J.h.d.J.h.J.h.d.J.h.d.J.h.k.h.f.k.h.f.k.h.
+[xk]/h.f.[zk]/h.f.[nk]/h.
+[JB]/h.d.J.h.d.J.h.J.h.d.J.h.d.J.h.k.h.f.k.h.f.k.h.
+[xk]/h.f.[zk]/h.f.k.h.J.h.d.J.h.d.J.h.
+J.G.d.J.G.d.J.G.J.G.S.J.G.S.J.G.d.
+J.G.d.J.G.S.L.J.G.z.J.G.L.J.G.z.J.
+G.L.J.G.L.J.G.L.J.G.L.J.G.L.J.G.L.`,
 };
 
 const ghostPlayTimeContainer = document.getElementById("ghost-play-time");
